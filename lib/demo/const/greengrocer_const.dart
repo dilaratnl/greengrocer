5efde36831c7485cd99c2c08aa4dfa13c10c27e4
Dilara <dilaratnl@hotmail.com>
2022-10-10 14:28:21 +0300

    grocerciesapp

---
+++ lib/demo/const/greengrocer_const.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class GreengrocerConst{
+  //images
+static const image_welcome = 'assets/images/fruit_welcome.png';
+static const image_zero = 'assets/images/strawberry.png';
+static const image_one = 'assets/images/fruit_zero.png';
+static const image_orange = 'assets/images/orange.png';
+static const image_lettuce = 'assets/images/lettuce.png';
+static const image_pepper = 'assets/images/pepper.png';
+static const image_potatoes = 'assets/images/potatoes.png';
+static const image_broccoli = 'assets/images/broccolii.png';
+static const image_lemon = 'assets/images/lemon.png';
+static const image_grape = 'assets/images/grape.png';
+static const image_domates = 'assets/images/domates_info.png';
+static const image_broccoli_one = 'assets/images/broccoli_one.png';
+static const image_meat = 'assets/images/meat.png';
+static const image_bananas = 'assets/images/bananas.png';
+
+
+
+//BorderRadius
+static BorderRadius borderRadiusCircular5 = BorderRadius.circular(5);
+static BorderRadius borderRadiusCircular10 = BorderRadius.circular(10);
+static BorderRadius borderRadiusCircular15 = BorderRadius.circular(15);
+static BorderRadius borderRadiusCircular20 = BorderRadius.circular(20);
+static BorderRadius borderRadiusCircular25 = BorderRadius.circular(25);
+static BorderRadius borderRadiusCircular30 = BorderRadius.circular(30);
+static BorderRadius borderRadiusCircular50 = BorderRadius.circular(50);
+
+//SizedBox
+static SizedBox sizedBoxHeight5 = SizedBox(height: 5);
+static SizedBox sizedBoxHeight10 = SizedBox(height: 10);
+static SizedBox sizedBoxHeight20 = SizedBox(height: 20);
+static SizedBox sizedBoxHeight30 = SizedBox(height: 30);
+static SizedBox sizedBoxHeight40 = SizedBox(height: 40);
+static SizedBox sizedBoxHeight60 = SizedBox(height: 60);
+
+static SizedBox sizedBoxHeight80 = SizedBox(height: 80);
+static SizedBox sizedBoxWidth15 = SizedBox(width: 15);
+static SizedBox sizedBoxWidth5 = SizedBox(width: 5);
+static SizedBox sizedBoxWidth10 = SizedBox(width: 10);
+static SizedBox sizedBoxWidth20 = SizedBox(width: 20);
+static SizedBox sizedBoxWidth25 = SizedBox(width: 25);
+//Text
+static const info_text = 'Find Your Daily \n Groceries Products';
+//static const info_text_zero = ' ';
+static const info_text_one = 'FLorem Ipsum is simply dummy text of the printing and typesetting industry';
+//static const info_text_two = '';
+static const info_text_button = 'Get Starded';
+static const info_text_location = 'Location';
+static const info_text_orderbest = 'Order Our Best \nGroceries Products';
+static const info_button_ordernow = 'Order Now';
+static const info_text_popular = 'Popular Items';
+static const info_text_see = 'See All';
+static const info_card_orange = 'Orange';
+static const info_card_quantity = '1 kg';
+static const info_card_price = '\$12';
+static const info_card_priceone = '\$15.00';
+static const info_card_strawberry= 'Strawberry';
+static const info_card_lettuce= 'Lettuce';
+static const info_card_pepper= 'Pepper';
+static const info_card_title= 'New Item';
+static const info_card_potatoes = 'Potatoes';
+static const info_card_broccoli = 'Broccoli';
+static const info_card_grape = 'Grape';
+static const info_card_lemon = 'Lemon';
+static const info_detail_text = 'Green Broccoli';
+static const info_detail = 'Lorem Ipsum is simpiy dummy text of the printing and teypesetting industry. Lorem Ipsum has been the ındustry standard dummy text ever since the 1500s. ';
+static const info_title = 'Description';
+static const info_menu = 'Related Item';
+static const info_card_meat = 'Meat';
+static const info_card_bananas = 'Bananas';
+static const info_cardAdd = 'Add To Card';
+
+// Color
+  static const color_white = Colors.white;
+  static const color_blueone = Color.fromARGB(255, 196, 224, 248);
+  static const color_black = Colors.black;
+  static const color_grey = Color.fromARGB(255, 135, 135, 135);
+  static const color_darkgrey = Color.fromARGB(255, 78, 78, 78);
+  static const color_blue = Colors.blue;
+  static const color_amber = Colors.amber;
+  static const color_green100 = Color.fromARGB(255, 200, 230, 201);
+  static const color_red100 = Color.fromARGB(255, 229, 64, 64);
+  static const color_green = Colors.green;
+  static const color_transparent = Colors.transparent;
+  static const color_lightgrey = Color.fromARGB(255, 221, 219, 219);
+  static const color_light_green = Color.fromARGB(255, 178, 220, 130);
+
+  //Padding
+  static const paddingGeneral20 = EdgeInsets.all(20);
+  static const paddingGeneral12 = EdgeInsets.all(12);
+  static const paddingGeneral15 = EdgeInsets.all(15);
+
+  static const paddingGeneronlyLeftRight10 = EdgeInsets.only(top: 60, left: 10, right: 10);
+  static const paddingGeneronlyLeftRightBottom20 = EdgeInsets.only(bottom: 30, left: 20, right: 20,top:20 );
+
+  //Radius
+   static const Radius radius10 = Radius.circular(10);
+
+
+}
